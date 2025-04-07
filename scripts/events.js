596989bed8133243dfb3ae032d9fd2b6723debe7
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2025-04-07 16:05:46 -0700

    Projects popdown

--- scripts/events.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".document .name").forEach(e => e.onclick = (ev) => {
-    let element = ev.currentTarget;
-    element.parentElement.classList.toggle("open")
-    element.querySelector("i").classList.toggle("fa-angle-right");
-    element.querySelector("i").classList.toggle("fa-angle-down");
-})
