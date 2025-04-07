50ef83d8b4f69d6762274622ac9a19256be4d896
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2025-04-07 10:37:55 -0700

    sponsorship email + patch with dev environ

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def page_events():
             yield {'event': os.path.splitext(file)[0]}
 
 if __name__ == "__main__":
-    if "dev" in sys.argv:
+    if os.environ["WAMODEV"]:
         app.run(port=8000)
     else:
         freezer.freeze()
