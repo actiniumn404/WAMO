debc31b9ac580b163764986a284d84d1b7a3fed0
infernoalt1 <infernoalternate1@gmail.com>
2025-04-08 17:16:38 -0700

    easdasjakds

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ def not_found():
     return render_template('404.html')
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     if "WAMODEV" in os.environ:
         app.run(port=80, host="0.0.0.0")
     else:
@@ -81,3 +82,7 @@ if __name__ == "__main__":
                 add_bg(dirpath + "/" + file, dirpath + "/" + file)
 
         freezer.freeze()
+=======
+    # freezer.freeze()
+    app.run(port=8000)
+>>>>>>> Stashed changes
